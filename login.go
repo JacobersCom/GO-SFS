73b83dc6fcf0a9e6f1b4dc191b2d1a378f95ec93
Hunter Wilkinson <jacobymartin1@icloud.com>
2026-07-27 15:51:50 -0400

    Fixing up error with getting the password

--- login.go
+++ login.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"crypto/rand"
+	"fmt"
 	"net/http"
+
+	"golang.org/x/crypto/argon2"
 )
 
 type argon_params struct {
@@ -12,7 +16,30 @@ type argon_params struct {
 	key_length  uint32
 }
 
-func login(w http.ResponseWriter, r *http.Request) {
+func GenerateRandomBytes(n uint32) ([]byte, error) {
+
+	b := make([]byte, n)
+	_, err := rand.Read(b)
+	check(err)
+
+	return b, nil
+}
+
+func GenerateFromPassword(password string, p *argon_params) (hash []byte, err error) {
+
+	//generate a cryptographically secure random salt.
+	salt, err := GenerateRandomBytes(p.salt_length)
+	check(err)
+
+	//generate the hash of the password using agron2id
+	hash = argon2.IDKey([]byte(password), salt, p.iterations, p.memory, p.parallelism, p.key_length)
+
+	fmt.Println("Password hashed")
+
+	return hash, nil
+}
+
+func Login(w http.ResponseWriter, r *http.Request) {
 
 	params := &argon_params{
 		memory:      64 * 1024,
@@ -30,7 +57,11 @@ func login(w http.ResponseWriter, r *http.Request) {
 	err := r.ParseForm()
 	check(err)
 
-	username := r.FormValue("first")
 	password := r.FormValue("password")
+
+	hash, err := GenerateFromPassword(password, params)
+	check(err)
+
+	fmt.Print(hash)
 
 }
--- main.go
+++ main.go
@@ -11,6 +11,9 @@ func server_start() {
 		http.ServeFile(w, r, "index.html")
 	})
 
+	mux := http.NewServeMux()
+	mux.HandleFunc("/login", Login)
+
 	fmt.Println("Starting server at :4321")
 	http.ListenAndServe(":4321", nil)
 }
